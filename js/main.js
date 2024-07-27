aa6d780617b516fc8227e8d5e2c54307eb87ee32
Brunohhla133 <brunohhla@gmail.com>
2024-07-26 23:06:53 -0300

    Creación del archivo index.html y Js, con array de objetos

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const prendas1 = [
+    {
+        id: 1,
+        nombre: "Remera White",
+        precio: 20,
+    },
+    {
+        id: 2,
+        nombre: "Remera Black",
+        precio: 20,
+    },
+    {
+        id: 3,
+        nombre: "Remera Orange",
+        precio: 20,
+    }
+]
+const prendas2 = [
+    {
+        id: 1,
+        nombre: "Pantalón White",
+        precio: 20,
+    },
+    {
+        id: 2,
+        nombre: "Pantalón Black",
+        precio: 20,
+    },
+    {
+        id: 3,
+        nombre: "Pantalón Orange",
+        precio: 20,
+    }
+]
+const prendas3 = [
+    {
+        id: 1,
+        nombre: "Campera White",
+        precio: 20,
+    },
+    {
+        id: 2,
+        nombre: "Campera Black",
+        precio: 20,
+    },
+    {
+        id: 3,
+        nombre: "Campera Orange",
+        precio: 20,
+    }
+]
